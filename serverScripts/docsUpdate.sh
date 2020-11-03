0b4e06b57615baad96fb710048457be1e0a6a682
jaykstah <jaykstah@gmail.com>
2020-11-03 15:45:54 -0800

    Fixed doc update to use -f flag with rm

--- serverScripts/docsUpdate.sh
+++ serverScripts/docsUpdate.sh
@@ -3,6 +3,6 @@ web=/var/www/html
 home=/home/jaykstah
 
 cd /home/jaykstah/git/balaclavabunker/ && git init && git pull
-sudo rm $web/docs/balaclavabunker.html.old
+sudo rm -f $web/docs/balaclavabunker.html.old
 sudo mv $web/docs/balaclavabunker.html $web/docs/balaclavabunker.html.old
 sudo cp $home/git/balaclavabunker/docs/balaclavabunker.html $web/docs/balaclavabunker.html
