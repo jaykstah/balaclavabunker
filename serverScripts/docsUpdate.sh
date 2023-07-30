ad792bb1c182dad3c9239a3e900ddc78c760286e
jaykstah <jaykstah@gmail.com>
2023-07-30 16:43:50 -0700

    Removed old documentation script

--- serverScripts/docsUpdate.sh
+++
@@ -1,8 +0,0 @@
-#!/bin/bash
-web=/var/www/html
-home=/home/jaykstah
-
-cd /home/jaykstah/git/balaclavabunker/ && git init && git pull
-sudo rm -f $web/docs/balaclavabunker.html.old
-sudo mv $web/docs/balaclavabunker.html $web/docs/balaclavabunker.html.old
-sudo cp $home/git/balaclavabunker/docs/balaclavabunker.html $web/docs/balaclavabunker.html
