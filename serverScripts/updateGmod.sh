9907b862fae7b0f62cd043a2bcfc509bfa4b22f2
jaykstah <jaykstah@gmail.com>
2023-07-30 16:58:23 -0700

    Make Gmod update script set install path before logging in

--- serverScripts/updateGmod.sh
+++ serverScripts/updateGmod.sh
@@ -1,3 +1,3 @@
 #!/bin/bash
 
-steamcmd +login anonymous +force_install_dir "/media/balaclavabunker/servers/garrys-mod-ds" +app_update 4020 validate +quit
+steamcmd +force_install_dir "/media/balaclavabunker/servers/garrys-mod-ds" +login anonymous +app_update 4020 validate +quit
